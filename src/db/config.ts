3669477be7a12f31be5ff6cb9b58fcc283211044
Matheus Estanislau <matheus.estanislau7@gmail.com>
2022-07-19 10:28:22 -0300

    chore: add orm configuration

--- src/db/config.ts
+++ src/db/config.ts
@@ -1,22 +1,14 @@
-import { DataSource, DataSourceOptions } from 'typeorm'
-import path from 'path'
+/* eslint-disable n/no-path-concat */
+import { DataSource } from 'typeorm'
 
-const connectOptions: DataSourceOptions = {
+export const dataSource = new DataSource({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: 'postgres',
   password: 'test',
   database: 'user',
-  synchronize: true,
-  entities: [`${path.join(__dirname, '/entities/*{.ts}')}`],
-  migrations: [`${path.join(__dirname, '/migrations/*{.ts}')}`]
-}
-
-export const DB = new DataSource(connectOptions)
-
-export const initDB = async () => {
-  const db = await DB.initialize()
-
-  return db
-}
+  migrationsRun: true,
+  entities: [`${__dirname}/entities/*{.ts, .js}`],
+  migrations: [`${__dirname}/migrations/*{.ts, .js}`]
+})
