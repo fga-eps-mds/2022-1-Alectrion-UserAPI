2097b4f006bc76ebdd7427c897c7a28b165ac1a5
MoacirMSJ <moacirmascarenha86@gmail.com>
2022-07-30 20:03:35 -0300

    refactor: atualizada utilização de envs

--- src/db/config.ts
+++ src/db/config.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm'
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
+  host: `${process.env.DB_HOST}`,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DATABASE,
+  username: `${process.env.DB_USER}`,
+  password: `${process.env.DB_PASS}`,
+  database: `${process.env.DATABASE}`,
   migrationsRun: true,
   entities: [`${__dirname}/entities/*{.ts, .js}`],
   migrations: [`${__dirname}/migrations/*{.ts, .js}`]
