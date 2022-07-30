50d1e63b73bd0447d3c0cd69c5cc6b706f199754
MoacirMSJ <moacirmascarenha86@gmail.com>
2022-07-30 16:13:52 -0300

    refactor: utiliza variaveis de ambiente na config

--- src/db/config.ts
+++ src/db/config.ts
@@ -3,11 +3,11 @@ import { DataSource } from 'typeorm'
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'test',
-  database: 'user',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DATABASE,
   migrationsRun: true,
   entities: [`${__dirname}/entities/*{.ts, .js}`],
   migrations: [`${__dirname}/migrations/*{.ts, .js}`]
