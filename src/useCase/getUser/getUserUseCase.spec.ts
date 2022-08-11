231492533d8f5e489b34cf49b248cc6e6d8d2122
IgorSPaiva <202016560@aluno.unb.br>
2022-08-11 00:57:31 -0300

    #120 test: adicionado teste para busca por id e erro

--- src/useCase/getUser/getUserUseCase.spec.ts
+++ src/useCase/getUser/getUserUseCase.spec.ts
@@ -1,10 +1,10 @@
-import { GetUserUseCase, GetUserError, FindUserInput } from "./getUserUseCase";
-import { mock } from "jest-mock-extended";
-import { Repository } from "../../repository/protocol/repository";
-import { User } from "../../domain/entities/user";
-import { Job } from "../../db/entities/userEnum/job";
-import { Role } from "../../db/entities/userEnum/role";
-import { datatype } from "faker";
+import { GetUserUseCase, GetUserError } from './getUserUseCase'
+import { mock } from 'jest-mock-extended'
+import { Repository } from '../../repository/protocol/repository'
+import { User } from '../../domain/entities/user'
+import { Job } from '../../db/entities/userEnum/job'
+import { Role } from '../../db/entities/userEnum/role'
+import { datatype } from 'faker'
 
 const mockedUser: User = {
   id: datatype.string(),
@@ -14,7 +14,7 @@ const mockedUser: User = {
   email: datatype.string(),
 
   username: datatype.string(),
-  
+
   job: Job.DELEGADO,
 
   role: Role.ADMIN,
@@ -23,28 +23,41 @@ const mockedUser: User = {
 
   createdAt: new Date(),
 
-  updatedAt: new Date(),
-};
+  updatedAt: new Date()
+}
 
-const repositoryMocked = mock<Repository>();
-const getUserUseCase = new GetUserUseCase(repositoryMocked);
+const repositoryMocked = mock<Repository>()
+const getUserUseCase = new GetUserUseCase(repositoryMocked)
 
-describe("Should test user use case to get user", () => {
-  it("should return an use with success when search by a userName", async () => {
+describe('Should test user use case to get user', () => {
+  it('should return an use with success when search by a userName', async () => {
     const mockedData = {
-      userName: "pedro",
-    };
-    repositoryMocked.findOneByUsername.mockResolvedValue(mockedUser);
-    const result = await getUserUseCase.execute(mockedData);
-    expect(result).toEqual({ isSuccess: true, data: mockedUser });
-  });
+      userName: 'pedro'
+    }
+    repositoryMocked.findOneByUsername.mockResolvedValue(mockedUser)
+    const result = await getUserUseCase.execute(mockedData)
+    expect(result).toEqual({ isSuccess: true, data: mockedUser })
+  })
 
-  it("should return an use with success when search by a email", async () => {
+  it('should return an use with success when search by a email', async () => {
     const mockedData = {
-      email: "joaopedro@email.com",
-    };
-    repositoryMocked.findOneByEmail.mockResolvedValue(mockedUser);
-    const result = await getUserUseCase.execute(mockedData);
-    expect(result).toEqual({ isSuccess: true, data: mockedUser });
-  });
-});
+      email: 'joaopedro@email.com'
+    }
+    repositoryMocked.findOneByEmail.mockResolvedValue(mockedUser)
+    const result = await getUserUseCase.execute(mockedData)
+    expect(result).toEqual({ isSuccess: true, data: mockedUser })
+  })
+  it('should return an use with success when search by a userId', async () => {
+    const mockedData = {
+      userId: mockedUser.id
+    }
+    repositoryMocked.findOne.mockResolvedValue(mockedUser)
+    const result = await getUserUseCase.execute(mockedData)
+    expect(result).toEqual({ isSuccess: true, data: mockedUser })
+  })
+  it('should return an error when params are incorrect', async () => {
+    const mockedData = {}
+    const result = await getUserUseCase.execute(mockedData)
+    expect(result).toEqual({ isSuccess: false, error: new GetUserError() })
+  })
+})
