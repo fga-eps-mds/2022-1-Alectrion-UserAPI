2bb0de94e80b3036d38ee11c48bf3a414390cc61
pedroblome <joaopedroblome78@gmail.com>
2022-08-10 23:02:13 -0300

    #120 refactor: atualizando useCase do factory

--- src/factories/useCases/getUser.ts
+++ src/factories/useCases/getUser.ts
@@ -1,21 +1,11 @@
 import UserRepository from '../../repository/userRepository'
-import { GetUserByIdUseCase, GetUserByEmailUseCase, GetUserByUsernameUseCase } from '../../useCase/getUser/getUserUseCase'
+import { GetUserUseCase } from '../../useCase/getUser/getUserUseCase'
 
-export const makeGetUserById = ()=>{
+export const makeGetUser = ()=>{
     const userRepository = new UserRepository()
-    return new GetUserByIdUseCase(userRepository)
+    return new GetUserUseCase(userRepository)
 }
 
-export const GetUserByEmail = ()=>{
-    const userRepository = new UserRepository()
-    return new GetUserByEmailUseCase(userRepository)
-}
-
-
-export const GetUserByUsername = ()=>{
-    const userRepository = new UserRepository()
-    return new GetUserByUsernameUseCase(userRepository)
-}
 
 
 
