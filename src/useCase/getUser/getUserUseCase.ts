425b6984f69863bd92c2469e16b955676af562b3
IgorSPaiva <202016560@aluno.unb.br>
2022-08-11 00:04:02 -0300

    #120 refactor: ajuste eslint

--- src/useCase/getUser/getUserUseCase.ts
+++ src/useCase/getUser/getUserUseCase.ts
@@ -1,48 +1,39 @@
-import { UseCase, UseCaseReponse } from "../protocols/useCase";
-import { Repository } from "../../repository/protocol/repository";
-import { User } from "../../domain/entities/user";
+import { UseCase, UseCaseReponse } from '../protocols/useCase'
+import { Repository } from '../../repository/protocol/repository'
+import { User } from '../../domain/entities/user'
 
 export class GetUserError extends Error {
   constructor() {
-    super("Não foi possivel encontrar o usuário!");
-    this.name = "GetUserError";
+    super('Não foi possivel encontrar o usuário!')
+    this.name = 'GetUserError'
   }
 }
 
 export interface FindUserInput {
-  userName? : string;
-  email? : string;
-  userId? : string;
-
+  userName?: string
+  email?: string
+  userId?: string
 }
 
-export class GetUserUseCase implements UseCase<{user: User}> {
+export class GetUserUseCase implements UseCase<{ user: User }> {
   constructor(private readonly userRepository: Repository) {}
-  async execute(userData: FindUserInput): Promise<UseCaseReponse<{ user: User }>> {
+  async execute(
+    userData: FindUserInput
+  ): Promise<UseCaseReponse<{ user: User }>> {
+    let userFound = null
 
-    let userFound = null;
-
-    if(userData.userName!){
-       userFound = await this.userRepository.findOneByUsername(userData.userName)
-
-    }
-    else if(userData.email!){
-       userFound = await this.userRepository.findOneByEmail(userData.email)
-    }
-    else if(userData.userId!){
-       userFound = await this.userRepository.findOne(userData.userId)
-
-    }
-    else{
+    if (userData.userName!) {
+      userFound = await this.userRepository.findOneByUsername(userData.userName)
+    } else if (userData.email!) {
+      userFound = await this.userRepository.findOneByEmail(userData.email)
+    } else if (userData.userId!) {
+      userFound = await this.userRepository.findOne(userData.userId)
+    } else {
       return {
         isSuccess: false,
-        error: new GetUserError(),
-      };
+        error: new GetUserError()
+      }
     }
-    return { isSuccess: true, data:  userFound }
-      
+    return { isSuccess: true, data: userFound }
   }
 }
-
-
-  
