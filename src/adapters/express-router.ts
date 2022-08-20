0d530fb0e985279b93527bf0f059c8a782b46bef
pedroblome <joaopedroblome78@gmail.com>
2022-08-20 13:14:21 -0300

    #119 refactor: mudança nas rotas

--- src/adapters/express-router.ts
+++ src/adapters/express-router.ts
@@ -6,7 +6,8 @@ type Adapter = (controller: Controller) => RequestHandler
 
 export const adaptExpressRoute: Adapter = (controller) => async (req, res) => {
   const { statusCode, data } = await controller.handle({
-    ...req.query
+    ...req.query,
+    ...req.body
   })
   const json = [200, 204].includes(statusCode) ? data : { error: data.message }
   res.status(statusCode).json(json)
