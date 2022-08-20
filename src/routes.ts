e986e1fca3ff6dcb614c76e852df2a45aab078c5
pedroblome <joaopedroblome78@gmail.com>
2022-08-19 23:30:22 -0300

    #119 refactor: variaveis e dependencias para essa issue

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,11 @@ import { Router } from 'express'
 import { adaptExpressRoute as adapt } from './adapters/express-router'
 import { makeUpdateUserController } from './factories/controllers/updateUser'
 import { makeGetUserController } from './factories/controllers/getUser'
+import { makeAuthenticateUserController } from './factories/controllers/authenticateUser'
 const routes = Router()
 
 routes.get('/update', adapt(makeUpdateUserController()))
 routes.post('/create', adapt(makeCreateUserController()))
 routes.get('/get', adapt(makeGetUserController()))
+routes.post('/login', adapt(makeAuthenticateUserController()))
 export default routes
