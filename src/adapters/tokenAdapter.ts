0b3306fe6b505861497771e314d8b2dbdf6d0072
pedroblome <joaopedroblome78@gmail.com>
2022-08-20 13:15:53 -0300

    #119 refactor: mudado o tipo de parametro para gerar token

--- src/adapters/tokenAdapter.ts
+++ src/adapters/tokenAdapter.ts
@@ -1,9 +1,8 @@
-import { hashSync } from "bcrypt";
 import { sign, Secret, SignOptions } from "jsonwebtoken";
 import { Token } from "../services/tokenGenerator";
 
 export class CreateToken implements Token {
-  generateToken(payload: String, secret: Secret): string {
+  generateToken(payload: object, secret: Secret): string {
     const token = sign(payload, secret);
 
     return token;
