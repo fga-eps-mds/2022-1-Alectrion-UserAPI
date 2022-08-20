ccb8eabde704dee13599fc61315a733c9edd5fe4
pedroblome <joaopedroblome78@gmail.com>
2022-08-19 21:49:49 -0300

    #119 refactor: Criação do adapter para checar a senha criptografada

--- src/adapters/bcryptAdapter.ts
+++ src/adapters/bcryptAdapter.ts
@@ -1,8 +1,15 @@
-import { hashSync } from 'bcrypt'
-import { Encryptor } from '../services/encryptor'
+import { compare, hashSync } from "bcrypt";
+import { Encryptor } from "../services/encryptor";
 
 export class BcryptAdapter implements Encryptor {
+  compare(passwordLogin: string, passwordDB: string):boolean {
+    let compareResult =  compare(passwordLogin, passwordDB )
+    if(!compareResult){
+      return true;
+    }
+    return false;
+  }
   encrypt(password: string) {
-    return hashSync(password, 3)
+    return hashSync(password, 3);
   }
 }
