bb32947808e30c0f620c5a0b3fe9b7845135a010
pedroblome <joaopedroblome78@gmail.com>
2022-08-22 19:34:02 -0300

    #119 refactor: Mudança de tipo de requisição

--- src/routes.ts
+++ src/routes.ts
@@ -4,10 +4,11 @@ import { adaptExpressRoute as adapt } from './adapters/express-router'
 import { makeUpdateUserController } from './factories/controllers/updateUser'
 import { makeGetUserController } from './factories/controllers/getUser'
 import { makeAuthenticateUserController } from './factories/controllers/authenticateUser'
+import { IsUserAuthenticated } from './middlewares/isUserAuthenticated'
 const routes = Router()
 
-routes.get('/update', adapt(makeUpdateUserController()))
+routes.put('/update', adapt(makeUpdateUserController()))
 routes.post('/create', adapt(makeCreateUserController()))
-routes.get('/get', adapt(makeGetUserController()))
+routes.get('/get',IsUserAuthenticated, adapt(makeGetUserController()))
 routes.post('/login', adapt(makeAuthenticateUserController()))
 export default routes
