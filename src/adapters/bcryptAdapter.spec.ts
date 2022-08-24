a481b9492d364797c0928e03fa9407f373fd0529
pedroblome <joaopedroblome78@gmail.com>
2022-08-24 01:05:17 -0300

    #119 refactor: Teste para verificação de senha

--- src/adapters/bcryptAdapter.spec.ts
+++ src/adapters/bcryptAdapter.spec.ts
@@ -1,9 +1,10 @@
 import { BcryptAdapter } from './bcryptAdapter'
-import { hashSync } from 'bcrypt'
+import { compareSync, hashSync } from 'bcrypt'
 
 jest.mock('bcrypt')
 
 const mockedDependency = jest.mocked(hashSync)
+const mockedDependencyCompare = jest.mocked(compareSync)
 
 describe('BcryptAdapter', () => {
   let sut: BcryptAdapter
@@ -17,4 +18,14 @@ describe('BcryptAdapter', () => {
     expect(mockedDependency).toHaveBeenCalledTimes(1)
     expect(mockedDependency).toHaveBeenCalledWith('any_password', 3)
   })
+
+  it('should compare 2 passwords and return a true if it are the same', () => {
+    const passwordLogin = "1234";
+    const passwordDB = sut.encrypt(passwordLogin)
+    sut.compare(passwordLogin,passwordDB )
+    expect(mockedDependencyCompare).toHaveBeenCalledTimes(1)
+    expect(mockedDependencyCompare).toHaveBeenCalledWith(passwordLogin, passwordDB)
+
+  })
+  
 })
